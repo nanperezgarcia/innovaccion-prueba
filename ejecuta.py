ea83256c6e87482f942ba4262a4d104264958a3d
nanperezgarcia <94764697+nanperezgarcia@users.noreply.github.com>
2022-02-13 18:46:07 -0600

    katas 1 y 2

---
+++ ejecuta.py
@@ -0,0 +1,44 @@
+
+#mensajes consola
+
+print("Hola Mundo")
+print("Lento pero seguro")
+print("vamos con todo")
+
+
+# variables
+
+division = 8/2
+resultado = division - 1
+print(resultado)
+
+
+                                    #Fechas del dia
+#------------------------------------------------------------------------------------------------------ 
+# Importamos la biblioteca 
+from datetime import date
+
+# Obtenemos la fecha de hoy
+date.today()
+
+# Mostramos la fecha en la consola
+print(date.today())
+
+
+print("Today's date is: " + str(date.today()))
+
+
+#----------------------------------------------------
+
+#programa inttoducir datos en consola
+
+print("Bienvenidos a innovaccion virtual launch X")
+nombre = input("introduce tu nombre : ")
+ciudad = input ("introduce tu ciudad : ")
+print("Soy " , nombre  ,"y ","vivo en " , ciudad)
+
+
+
+#-----------------------------------------------
+
+
